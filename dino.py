6139447a4c39f9047eb1d9c5cb1ae4657293be96
JacobBerkey <jacobberkey@yahoo.com>
2021-09-09 13:23:06 -0400

    Weapon class and dino class created

---
+++ dino.py
@@ -0,0 +1,8 @@
+class Dinosaur:
+    def __init__(self, name, attack_power):
+        self.dino_list = name["Ducky", "Littlefoot", "The Sharptooth"]
+        self.attack_power = attack_power[50, 65, 45]
+        self.health = [200, 150, 300]
+
+    def attack(self, robot):
+        pass
---
+++ weapon.py
@@ -0,0 +1,4 @@
+class Weapons:
+    def __init__(self, name, attack_power):
+        self.name = name['Blade storm', 'Elders Flame', 'Operator', 'Hunters Fury', 'Show Stopper', 'Phantom']
+        self.attack_power = attack_power[60, 40, 120, 40, 80, 30]
