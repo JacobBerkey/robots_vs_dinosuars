c22bc02db16edb150959c8fd691ac9c480484d3e
JacobBerkey <jacobberkey@yahoo.com>
2021-09-10 10:09:32 -0400

    All methods in battlefield completed

--- battlefield.py
+++ battlefield.py
@@ -8,9 +8,11 @@ from dinosaur import dinosaur
 class battlefield:
     def __init__(self):
         self.run_game()
-        self.show_dino_opponent_option()
-        self.dinosaurs = herd()
-
+        self.battle()
+        self.dino_turn()
+        self.robot_turn()
+        self.display_winners()
+        
 
 
     def run_game(self):
@@ -25,22 +27,55 @@ class battlefield:
         self.battle()
 
     def battle(self):
-        pass
+        self.dinosaurs = herd()
+        self.robots = fleet()
+        self.input = input("Choose 'Dinosaurs' or 'Robots'")
+        if self.input == "Dinosaurs":
+            herd()
+            for dinosaur in self.dinosaurs.dinosaurs:
+                print("Name:"f"{dinosaur.name}")
+                print("Attack Power:"f"{dinosaur.attack_power}")
+                print("Health:"f"{dinosaur.health}")
+                self.show_dino_opponent_option()
 
-    def dino_turn(self, dinosaur):
-        pass
+        if self.input != "Dinosaurs":
+            fleet()
+            for robot in self.robots.robots:
+                print("Name:"f"{robot.name}")
+                print("Weapon Name:"f"{robot.weapon.name}")
+                print("Weapon Damage:"f"{robot.weapon.attack_power}")
+                print("Health:"f"{robot.health}")
+                self.show_robo_opponent_option()
 
-    def robot_turn(self, robot):
-        pass
+
+    def dino_turn(self, Dinosaur):
+       Dinosaur = Dinosaur
+       dinosaur()
+
+    def robot_turn(self, Robot):
+        Robot = Robot
+        robot()
 
     def show_dino_opponent_option(self):
-        for dinosaur in self.dinosaurs.dinosaurs:
-            print(f"{dinosaur.health}")
-
+        for robot in self.robots.robots:
+            print("Name:"f"{robot.name}")
+            print("Weapon Name:"f"{robot.weapon.name}")
+            print("Weapon Damage:"f"{robot.weapon.attack_power}")
+            print("Health:"f"{robot.health}")
+        
     def show_robo_opponent_option(self):
-        for robot in self.robot.robot:
-            print(f"{robot.weapons}")
+        for dinosaur in self.dinosaurs.dinosaurs:
+            print("Name:"f"{dinosaur.name}")
+            print("Attack Power:"f"{dinosaur.attack_power}")
+            print("Health:"f"{dinosaur.health}")
+
 
     def display_winners(self):
-        pass
+        for dinosaur in self.dinosaurs.dinosaurs:
+            if dinosaur.health == 0:
+                print("Robots Win!!!")
+
+        for robot in self.robots.robots:
+            if robot.health == 0:
+                print("Dinosaurs Win")
 
