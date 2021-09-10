c30416c6952cf02c93fc1a336dc677c75c87f515
JacobBerkey <jacobberkey@yahoo.com>
2021-09-10 10:04:04 -0400

    Attack for robot and attack for dinosaur finished

--- dinosaur.py
+++ dinosaur.py
@@ -1,3 +1,7 @@
+from fleet import fleet
+from herd import herd
+
+
 class dinosaur:
     def __init__(self, name, attack_power):
         self.name = name
@@ -5,4 +9,23 @@ class dinosaur:
         self.health = 500
 
     def attack(self, robot):
-        pass
+        self.robots = fleet()
+        for robot in self.robots.robots:
+            print(f"{robot.health}")
+
+        self.dinosaurs = herd()
+        for dinosaur in self.dinosaurs.dinosaurs:
+                print("Name:"f"{dinosaur.name}")
+                print("Attack Power:"f"{dinosaur.attack_power}")
+
+        
+        self.input = input("Type 'attack' to damage opponent")
+        if self.input == "attack":
+            print("You attacked:" f"{robot.name}" "for" f"{dinosaur.attack_power}""Damage!")
+            print(f"{robot.health}" - f"{dinosaur.attack_power}")
+
+
+        
+        
+
+
--- robot.py
+++ robot.py
@@ -1,13 +1,30 @@
 from weapon import weapon
+from herd import herd
+from fleet import fleet
 
 
 class robot:
     def __init__(self, name):
         self.name = name
         self.health = 500
-        self.weapon = [weapon("Blade Storm", attack_power= 60), weapon("Phantom", attack_power=40), weapon("Operator", attack_power=80)]
+        self.weapon = weapon("Blade Storm", attack_power= 60)
 
 
 
     def attack(self, dinosaur):
-        pass
+        self.dinosaurs = herd()
+        for dinosaur in self.dinosaurs.dinosaurs:
+            print(f"{dinosaur.name}")
+            print(f"{dinosaur.attack_power}")
+
+        self.robots = fleet()
+        for robot in self.robots.robots:
+            print("Name:"f"{robot.name}")
+            print("Weapon Name:"f"{robot.weapon.name}")
+            print("Weapon Power:"f"{robot.weapon.attack_power}")
+
+            print("You attacked:" f"{dinosaur.name}" "for" f"{robot.weapon.attack_power}""Damage!")
+            self.input = input("Type 'attack' to damage opponent")
+        if self.input == "attack":
+            print("You attacked:" f"{dinosaur.name}" "for" f"{robot.weapon.attack_power}""Damage!")
+            print(f"{dinosaur.health}" - f"{robot.weapon.attack_power}")
